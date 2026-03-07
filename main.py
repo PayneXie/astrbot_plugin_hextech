7caf216b1f7f7af5efa5f12be345bd88e8df7390
PayneXie <payne9586@gmail.com>
2026-03-07 14:03:21 +0800

    feat: 实现海克斯大乱斗信息差查询插件基础功能
    - 将插件元数据更新为海克斯大乱斗信息差插件
    - 替换原示例指令为查询英雄的`/海斗`指令
    - 调整插件名称、描述、作者及仓库地址以匹配新插件

--- main.py
+++ main.py
@@ -10,15 +10,10 @@ class MyPlugin(Star):
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    @filter.command("海斗")
+    async def haidou(self, event: AstrMessageEvent, hero_name: str):
+        """查询英雄"""
+        yield event.plain_result(f"英雄: {hero_name}")
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
